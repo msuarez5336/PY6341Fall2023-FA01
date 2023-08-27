f7c8a66ba2c6a8a09cc15b1a0526cb281a6dc143
Miguel Suarez <suarezlozanomi@outlook.com>
2023-08-27 18:06:57 -0500

    Second Draft

--- Assignments/Assignment_1/helloworld.py
+++ Assignments/Assignment_1/helloworld.py
@@ -1,3 +1,10 @@
+# Full Name: Miguel Suarez
+# Student ID: 1045336
+# Assignment Due Date: 08/27/2023
+# MSITM 6341: FA01
+# Assignment #1
+# Assignment Title: Hello World
+# Helloworld.py
 name = "Miguel Suarez"
-output = f"hello world my name is {name} and I am a Python student"
+output = f"\n\n Hello World!\n\n My name is {name}.\n\n I am a Python student.\n\n Have a nice day!"
 print(output)
