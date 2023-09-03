56a390c603d67d4bef4315c41b0b1b179c3f22bb
Miguel Suarez <suarezlozanomi@outlook.com>
2023-09-03 01:50:38 -0500

    Final Draft Assignment 2

---
+++ Assignments/Asignment_2/String_Number_Functions.py
@@ -0,0 +1,33 @@
+# Full Name: Miguel Suarez
+# Student ID: 1045336
+# Assignment Due Date: 09/03/2023
+# MSITM 6341: FA01
+# Assignment #2
+# Assignment Title: String_Number_Functions
+# String_Number_Functions.py
+value1 = 50
+value2 = 53
+
+total_value = value1 + value2
+
+print("==============================")
+
+print("Results Dashboard")
+
+print("------------------------------")
+
+print("Value 1:", value1)
+
+print()
+
+print("Value 2:", value2)
+
+print("------------------------------")
+
+print("Total summary: ${:.2f}".format(total_value))
+
+result = total_value / 7.6
+
+print("Result: ${:.2f}".format(result))
+
+print("==============================")
